d5d34c2715eda30b5e34bbf3f5a9c5e16d515a81
Sarah Rady Sayed <87918461+sarahhrady@users.noreply.github.com>
2024-04-29 23:08:33 +0300

    Time: 106 ms (71.65%) | Memory: 91.1 MB (100.00%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -7,32 +7,16 @@ public:
             sum^=nums[i];
         }
         if(sum==k)return 0;
-        vector<int>bits(31,0);
+        //vector<int>bits(31,0);
+        int cnt=0;
         for(int i=0; i<32; i++)
         {
             if(((sum^k)>>i)&1)
             {
-               /* if(!((k>>i)&1))
-                {
-                int ones=0;
-                for(int num:nums)
-                {
-                    if((num>>i)&1)ones++;
-                }
-                int zeros=nums.size()-ones;
-                bits[i]=min(ones,zeros);
-                }
-                else*/
-                {
-                    bits[i]=1;
-                }
+                cnt+=1; 
             }
         }
-        int cnt=0;
-        for(int i=0; i<bits.size(); i++)
-        {
-            cnt+=bits[i];
-        }
+       
         return cnt;
     }
 };
