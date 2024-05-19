17fbf16c44c12498b2726d484b9362b12ba3c981
Sarah Rady Sayed <87918461+sarahhrady@users.noreply.github.com>
2024-05-19 03:36:18 +0300

    Time: 553 ms (5.00%) | Memory: 32 MB (49.73%) - LeetSync

---
+++ 3308-apply-operations-to-make-string-empty/apply-operations-to-make-string-empty.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string lastNonEmptyString(string s) {
+        map<char, int> m;
+        for (char c : s) m[c]++;
+        
+        string res = "";
+        int maxx = 0;
+        for (auto it : m) {
+            maxx = max(maxx, it.second);
+        }
+
+        set<char> se;
+        for (int i = 0; i < s.size(); i++) {
+            if (m[s[i]] == maxx) se.insert(s[i]);
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (m[s[i]] == maxx) {
+                cnt++;
+                m[s[i]] = 0;
+            }
+        }
+
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (cnt == 0) break;
+            if (se.count(s[i]) > 0) {
+                res.push_back(s[i]);
+                se.erase(s[i]);
+                cnt--;
+            }
+        }
+
+        /*for (char c : s) {
+            if (se.count(c) > 0) res.push_back(c);
+        }*/
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
