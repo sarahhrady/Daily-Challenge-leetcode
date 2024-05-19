86652848a71a460605499fd6aada0657afa46413
Sarah Rady Sayed <87918461+sarahhrady@users.noreply.github.com>
2024-05-19 03:41:11 +0300

    Time: 401 ms (10.68%) | Memory: 32 MB (45.00%) - LeetSync

--- 3308-apply-operations-to-make-string-empty/apply-operations-to-make-string-empty.cpp
+++ 3308-apply-operations-to-make-string-empty/apply-operations-to-make-string-empty.cpp
@@ -15,27 +15,22 @@ public:
             if (m[s[i]] == maxx) se.insert(s[i]);
         }
 
-        int cnt = 0;
+        /*int cnt = 0;
         for (int i = 0; i < s.size(); i++) {
             if (m[s[i]] == maxx) {
                 cnt++;
                 m[s[i]] = 0;
             }
-        }
+        }*/
 
         for (int i = s.size() - 1; i >= 0; i--) {
-            if (cnt == 0) break;
+            //if (cnt == 0) break;
             if (se.count(s[i]) > 0) {
                 res.push_back(s[i]);
                 se.erase(s[i]);
-                cnt--;
+               // cnt--;
             }
         }
-
-        /*for (char c : s) {
-            if (se.count(c) > 0) res.push_back(c);
-        }*/
-
         reverse(res.begin(), res.end());
         return res;
     }
